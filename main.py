0fdadc45364663b090bcccc4756f4603a9bb4c83
gustavoio <gustavo.pinho.pt@gmail.com>
2023-08-06 21:00:17 +0100

    HomeAssistant integration

--- main.py
+++ main.py
@@ -5,31 +5,15 @@ import json
 import os
 import gpiozero
 
-def loadUniqueId(uniqueIdFile):
-    file = open(uniqueIdFile, "r")
-    uniqueId = file.read().rstrip()
-    file.close()
-    return uniqueId
+def publishDiscoveryMessage(client, zone, zoneData):
+    payload = json.dumps(zoneData['description'])
+    client.publish(zoneData['discovery_topic'], payload, 0, retain=True)
 
-def loadSettings(settingsFile):
-    file = open(settingsFile, "r")
-    settings = json.loads(file.read())
-    file.close()
-    return settings
+def publishAvailability(client, zone, zoneData, availability):
+    client.publish(zoneData['description']['availability']['topic'], availability, 0, retain=True)
 
-def updateSettings(settingsJson, settingsFile):
-    file = open(settingsFile, "w")
-    file.write(settingsJson)
-    file.close()
-    os._exit(1) # restart app to load new settings
-
-def publishDescription(client, topic, description):
-    s = json.dumps(description)
-    client.publish(topic, s, 0, retain=True)
-
-def publishSettings(client, topic, settings):
-    s = json.dumps(settings)
-    client.publish(topic, s, 0, retain=True)
+def publishState(client, zone, zoneData, state):
+    client.publish(zoneData['description']['state_topic'], state, 0, retain=True)
 
 def turnAllZonesOff(zones):
     for zone in zones:
@@ -38,130 +22,85 @@ def turnAllZonesOff(zones):
 def turnZoneOff(zone):
     print("Turning zone " + str(zone["gpio"]) + " off")
     zone["gpio"].off()
+    zone['state'] = 'off'
 
 def turnZoneOn(zone):
     print("Turning zone " + str(zone["gpio"]) + " on")
     zone["gpio"].on()
+    zone['state'] = 'on'
 
 def onConnect(client, userdata, flags, rc):
     print("Connected: " + str(rc))
+    zones = userdata
 
-    # subscribe
-    client.subscribe(settingsUpdateTopic)
-    client.subscribe(zone1Topic)
-    client.subscribe(zone2Topic)
-    client.subscribe(zone3Topic)
-    client.subscribe(zone4Topic)
-    
+    for zone in zones:
+        # subscribe
+        client.subscribe(zones[zone]['description']['command_topic'])
+
+        publishDiscoveryMessage(client, zone, zones[zone])
+        publishAvailability(client, zone, zones[zone], 'online')
+        publishState(client, zone, zones[zone], zones[zone]['state'])
 
 def onMessage(client, userdata, msg):
     topic = msg.topic
     payload = msg.payload.decode("utf-8")
     print(topic + " " + payload)
 
-    if topic == settingsUpdateTopic:
-        updateSettings(payload, settingsFile)
-    elif topic == zone1Topic:
-        if payload == "on":
-            turnZoneOn(zones["zone1"])
-        elif  payload == "off":
-            turnZoneOff(zones["zone1"])
-    elif topic == zone2Topic:
-        if payload == "on":
-            turnZoneOn(zones["zone2"])
-        elif  payload == "off":
-            turnZoneOff(zones["zone2"])
-    elif topic == zone3Topic:
-        if payload == "on":
-            turnZoneOn(zones["zone3"])
-        elif  payload == "off":
-            turnZoneOff(zones["zone3"])
-    elif topic == zone4Topic:
-        if payload == "on":
-            turnZoneOn(zones["zone4"])
-        elif  payload == "off":
-            turnZoneOff(zones["zone4"])
-    else:
-        print("Unexpected topic: " + msg.topic)
+    pieces = topic.split('/')
+    if pieces[3] == 'set':
+        zone = pieces[2]
+        if payload == 'on':
+           turnZoneOn(zones[zone])
+        elif payload == 'off':
+           turnZoneOff(zones[zone])
+        publishState(client, zone, zones[zone], zones[zone]['state'])
 
 # setup
-client = mqtt.Client()
-client.on_connect = onConnect
-client.on_message = onMessage
-
-uniqueIdFile = "/home/pi/irrigation/unique_id"
-settingsFile = "/home/pi/irrigation/irrigation_settings.json"
-
-version = "0.1.0"
-
-uniqueId = loadUniqueId(uniqueIdFile)
-descriptionTopic = "devices/<id>/description".replace("<id>", uniqueId)
-settingsTopic = "devices/<id>/settings".replace("<id>", uniqueId)
-settingsUpdateTopic = settingsTopic + "/update"
-zone1Topic = "devices/<id>/zone/1".replace("<id>", uniqueId)
-zone2Topic = "devices/<id>/zone/2".replace("<id>", uniqueId)
-zone3Topic = "devices/<id>/zone/3".replace("<id>", uniqueId)
-zone4Topic = "devices/<id>/zone/4".replace("<id>", uniqueId)
-
-timeFormat = "%Y-%m-%d %H:%M:%S"
-startupTime = datetime.now()
-
-description = {
-    "id": uniqueId,
-    "type": "irrigatonContoller",
-    "version": version,
-    "uptime": 0,
-    "time": startupTime.strftime(timeFormat)
-}
-
-settings = loadSettings(settingsFile)
+version = "2.0.0"
 
 zones = {
-    "zone1": {
+    "irrigation1": {
         "gpio": gpiozero.OutputDevice(27, False)
     },
-    "zone2": {
+    "irrigation2": {
         "gpio": gpiozero.OutputDevice(22, False)
     },
-    "zone3": {
+    "irrigation3": {
         "gpio": gpiozero.OutputDevice(24, False)
     },
-    "zone4": {
+    "irrigation4": {
         "gpio": gpiozero.OutputDevice(23, False)
+    },
+    "irrigation5": {
+        "gpio": gpiozero.OutputDevice(25, True)
     }
 }
 
-client.connect("localhost", 1883)
-client.loop_start()
+turnAllZonesOff(zones)
 
-publishSettings(client, settingsTopic, settings)
+# fill description
+for zone in zones:
+    zones[zone]['description'] = {}
+    zones[zone]['description']['device'] = {"identifiers": ['irrigation-controller']}
+    zones[zone]['description']['name'] = zone
+    zones[zone]['description']['device_class'] = 'switch'
+    zones[zone]['description']['payload_on'] = 'on'
+    zones[zone]['description']['payload_off'] = 'off'
+    zones[zone]['description']['state_on'] = 'on'
+    zones[zone]['description']['state_off'] = 'off'
+    zones[zone]['description']['availability'] = {}
+    zones[zone]['description']['availability']['payload_available'] = 'online'
+    zones[zone]['description']['availability']['payload_not_available'] = 'offline'
+    zones[zone]['description']['availability']['topic'] = 'homeassistant/switch/' + zone + '/available'
+    zones[zone]['discovery_topic'] = 'homeassistant/switch/' + zone + '/config'
+    zones[zone]['description']['command_topic'] = 'homeassistant/switch/' + zone + '/set'
+    zones[zone]['description']['state_topic'] = 'homeassistant/switch/' + zone + '/state'
+    zones[zone]['description']['unique_id'] = version+zone
 
-while True:
-
-    now = datetime.now()
-    dayOfWeek = now.weekday()
-    hour = now.hour
-    minute = now.minute
-
-    # update desciption
-    description["uptime"] = (now - startupTime).total_seconds()
-    description["time"] = now.strftime(timeFormat)
-
-    publishDescription(client, descriptionTopic, description)
-
-    # for each zone check if the schedule matches the status
-    for zone in zones:
-        for schedule in settings["schedules"][zone]:
-
-            if dayOfWeek == schedule["dayOfWeek"] and \
-                hour == schedule["hour"] and \
-                minute == schedule["minute"]:
-
-                if schedule["action"] == "on":
-                    turnZoneOn(zones[zone])
-                elif schedule["action"] == "off":
-                    turnZoneOff(zones[zone])
-
-    time.sleep(30) # sleep for 30 seconds so schedules are verified twice a minute
+client = mqtt.Client(userdata=zones)
+client.on_connect = onConnect
+client.on_message = onMessage
 
 
+client.connect("192.168.1.201", 1883)
+client.loop_forever()
